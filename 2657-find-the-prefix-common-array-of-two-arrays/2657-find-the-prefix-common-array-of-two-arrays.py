6a01a99ed8345a1cd56246b481ff2bebafe02caa
NIloy Blueee <143703109+niloyblueee@users.noreply.github.com>
2025-03-18 23:40:40 +0600

    Time: 61 ms (10.98%), Space: 17.7 MB (91.98%) - LeetHub

--- 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.py
+++ 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.py
@@ -2,10 +2,6 @@ class Solution:
     def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
         c = []
         for i in  range(len(A)):
-            if i == 0:
-                if A[i] == B[i]:
-                    c.append(1)
-                    continue
 
             check =  A[0:i+1]
             check2 = B[0:i+1]
